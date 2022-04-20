466ae51f40f97203777448961a65102a3351e94e
nonsense2596 <peter.szatmary@windowslive.com>
2022-04-20 11:44:00 +0200

    Displaying available storage in the navbar at all times

---
+++ nSeed/Global/Utils/SystemInformation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nSeed.Global.Utils
+{
+    public class SystemInformation
+    {
+        public static string PrintDiskSpace(string driveLetter = "C:\\")
+        {
+            DriveInfo drive = new DriveInfo(driveLetter);
+
+            var totalBytes = drive.TotalSize;
+            var freeBytes = drive.AvailableFreeSpace;
+
+            var freePercent = (int)((100 * freeBytes) / totalBytes);
+
+            long totalBytesGb = (long)(totalBytes / Math.Pow(1024, 3));
+            long freeBytesGb = (long)(freeBytes / Math.Pow(1024, 3));
+
+            return String.Format("{0}Gb/{1}Gb ({2}% free)", totalBytesGb, freeBytesGb, freePercent);
+        }
+    }
+}
---
+++ nSeed/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nSeed.ViewComponents
+{
+    public class HeaderViewComponent: ViewComponent
+    {
+        private readonly long kex;
+
+        public HeaderViewComponent()
+        {
+            this.kex = new DriveInfo("C:\\").AvailableFreeSpace;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewData["StorageInfo"] = Global.Utils.SystemInformation.PrintDiskSpace();
+            return View("Default");
+            //return await Task.FromResult((IViewComponentResult)View("/Views/Shared/Components/Test/Default"));
+        }
+    }
+}
+
+
+// https://docs.microsoft.com/en-us/answers/questions/343958/how-to-check-avilable-free-disk-space-drive-on-lin.html
+// https://stackoverflow.com/questions/51390486/net-core-find-free-disk-space-on-different-os
