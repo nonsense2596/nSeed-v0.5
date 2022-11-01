9939a5ca714e44334798b6cd48e2ba0ed2360775
nonsense2596 <peter.szatmary@windowslive.com>
2022-11-01 18:30:29 +0100

    Implemented torrents file reader class

---
+++ nSeed/Models/TorrentFileData.cs
@@ -0,0 +1,14 @@
+﻿namespace nSeed.Models
+{
+    public class TorrentFileData
+    {
+        public TorrentFileData(string name, string size)
+        {
+            Name = name;
+            Size = size;
+        }
+
+        public string Name { get; set; }
+        public string Size { get; set; }
+    }
+}
